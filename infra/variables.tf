0c15e274b1551d3489428476c6af5378ec3e80da
Baskar <119995743+Baskarbalakrishnan@users.noreply.github.com>
2025-09-13 02:37:18 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,18 @@
-variable "aws_region"        { type = string }
-variable "instance_type"     { type = string  default = "t3.micro" }
-variable "key_name"          { type = string }
-variable "allowed_ssh_cidr"  { type = string  default = "0.0.0.0/0" } # ideally set to Jenkins IP/32
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
 
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "allowed_ssh_cidr" {
+  type    = string
+  default = "YOUR_JENKINS_PUBLIC_IP/32" # change this
+}
